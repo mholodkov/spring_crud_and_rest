1816021c902295ba1a2afec3ef3d98dc4e078a42
Maksim Kholodkov <mholodkov@MacBook-Air-Maksim.local>
2020-05-26 22:14:42 +0300

    new files

---
+++ src/main/java/com/example/demo/repos/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repos;
+
+import com.example.demo.model.User;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface UserRepository extends PagingAndSortingRepository<User, Long>,
+        JpaSpecificationExecutor<User> {
+}
