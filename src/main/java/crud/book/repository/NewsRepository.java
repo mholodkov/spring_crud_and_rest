62c9416c85ad5d2a7559a0a2940f56d5fa0d2c9c
Jura Kuznetsov <yk@finch-melrose.com>
2020-05-28 00:05:13 +0300

    refacoring

--- src/main/java/crud/book/repository/NewsRepository.java
+++ src/main/java/crud/book/repository/NewsRepository.java
@@ -1,12 +1,12 @@
 package crud.book.repository;
 
 import crud.book.model.News;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-import java.util.List;
+public interface NewsRepository extends JpaRepository<News, Long> {
 
-public interface NewsRepository extends PagingAndSortingRepository<News, Long>,
-        JpaSpecificationExecutor<News> {
-    List<News> findByTitle(String title);
+  List<News> findByTitle(String title);
 }
